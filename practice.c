ccef93eff17bc44c02d1ebe5f3b5323f186e71c6
UMM E HANI <k253048@nu.edu.pk>
2025-11-04 15:24:54 +0500

    Add files via upload

---
+++ practice.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    char hy[25];
+    int i, temp;
+    int length = 0;
+    printf("Enter a string: ");
+    fgets(hy, sizeof(hy), stdin);
+    while (hy[length] != '\0') {
+        length++;
+    }
+    for (i = 0; i < length / 2; i++) {
+        temp = hy[i];
+        hy[i] = hy[length - i - 2]; 
+        hy[length - i - 2] = temp;
+    }
+    printf("Reversed string: %s\n", hy);
+    return 0;
+}
+
